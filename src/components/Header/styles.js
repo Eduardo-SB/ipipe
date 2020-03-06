a664b18da866bf03ef0bbc5d88d78c114c197fa0
Eduardo SB <eduardosb95@gmail.com>
2020-03-05 22:03:47 -0300

    Commit project

---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 80px;
+    width: 100%;
+    background: #41bbf0;
+    color: #FFF;
+
+    display: flex;
+    align-items: center;
+
+    /* Define */
+    position: fixed;
+    padding: 0 30px;
+`;
